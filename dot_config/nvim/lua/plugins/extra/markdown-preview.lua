f0b595323c864413d81477ffc4ed75f6ceb95166
Jiandong Qiu <qiujiandong@nucleisys.com>
2026-06-29 09:35:06 +0800

    nvim: add markdown-preview and update lock.json
    Signed-off-by: Jiandong Qiu <qiujiandong@nucleisys.com>

---
+++ dot_config/nvim/lua/plugins/extra/markdown-preview.lua
@@ -0,0 +1,15 @@
+return {
+  'iamcco/markdown-preview.nvim',
+  event = 'VeryLazy',
+  cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+  ft = { 'markdown' },
+  init = function()
+    vim.cmd [[
+    function! OpenBrowserWSL(url)
+      execute 'silent !wslview ' . shellescape(a:url)
+    endfunction
+    ]]
+    vim.g.mkdp_browserfunc = 'OpenBrowserWSL'
+  end,
+  -- build = function() vim.fn['mkdp#util#install']() end,
+}
