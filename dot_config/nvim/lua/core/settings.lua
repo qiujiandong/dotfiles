1d1030374661e806fca8509789f5d33dda64d9ff
Jiandong Qiu <qiujiandong1998@gmail.com>
2026-07-05 13:52:31 +0800

    nvim: enable treesitter folding defaults
    Set expr-based folding with treesitter and start with folds expanded.
    
    Signed-off-by: Jiandong Qiu <qiujiandong1998@gmail.com>

--- dot_config/nvim/lua/core/settings.lua
+++ dot_config/nvim/lua/core/settings.lua
@@ -109,3 +109,10 @@ vim.filetype.add {
     config = 'kconfig',
   },
 }
+
+vim.o.foldenable = true
+vim.o.foldmethod = 'expr'
+vim.o.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+vim.o.foldlevel = 99
+vim.o.foldlevelstart = 1
+vim.o.foldtext = ''
